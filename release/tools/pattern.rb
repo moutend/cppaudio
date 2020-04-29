7935a207f1daa4af484e40b56f85af8955267da9
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-04-29 20:54:10 +0900

    chore

--- release/tools/pattern.rb
+++ release/tools/pattern.rb
@@ -1,10 +1,22 @@
 #!/usr/bin/env ruby
 
-# ruby pattern.rb > tests.csv
-
 puts "template_name,source_samples_per_sec,source_channels,source_bits_per_sample,target_samples_per_sec,target_channels,target_bits_per_sample"
 
-["TestFadeLauncherEngine"].each do |template_name|
+[
+  # "TestNormalLauncherEngine",
+  # "TestInterruptLauncherEngine",
+  # "TestFadeLauncherEngine",
+  # "TestBlankLauncherEngine",
+  "TestStutterLauncherEngine",
+  # "TestFormatLauncherEngine",
+  # "TestDelayLauncherEngine",
+  # "TestNormalRingEngine",
+  # "TestFadeRingEngine",
+  # "TestBlankRingEngine",
+  # "TestStutterRingEngine",
+  # "TestFormatRingEngine",
+  # "TestDelayRingEngine"
+].each do |template_name|
   [44100, 48000, 96000].each do |source_samples_per_sec|
     [1,2].each do |source_channels|
       [16, 24, 32].each do |source_bits_per_sample|
