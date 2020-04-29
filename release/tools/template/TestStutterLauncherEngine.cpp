d6c569256fee984c71b6820f020c5b77d7f54eed
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-04-29 11:24:31 +0900

    Update

---
+++ release/tools/template/TestStutterLauncherEngine.cpp
@@ -0,0 +1,52 @@
+#include <cppaudio/engine.h>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+
+int main() {
+  std::ifstream input("__INPUT_FILE__", std::ios::binary | std::ios::in);
+
+  if (!input.is_open()) {
+    printf("Failed to open '__INPUT_FILE__'.\n");
+
+    return -1;
+  }
+
+  int16_t outputBytesPerSample = 4; // Always fixed to 32 bit.
+  int16_t outputChannels = __OUTPUT_CHANNELS__;
+  int32_t outputSamplesPerSec = __OUTPUT_SAMPLES_PER_SEC__;
+
+  // Create an audio file whic duration is 3 sec.
+  int32_t outputSamples = outputChannels * outputSamplesPerSec * 3;
+  char *pData = new char[outputSamples * outputBytesPerSample]{};
+
+  PCMAudio::LauncherEngine *engine = new PCMAudio::LauncherEngine(10, 32);
+  engine->SetFormat(outputChannels, outputSamplesPerSec);
+  engine->Register(0, input);
+
+  input.close();
+
+  for (int i = 0; i < outputSamples; i++) {
+    if (i % (outputSamplesPerSec * outputChannels / 2) == 0) {
+      engine->Feed(0);
+    }
+
+    int32_t s32 = engine->Read();
+
+    for (int j = 0; j < outputBytesPerSample; j++) {
+      pData[outputBytesPerSample * i + outputBytesPerSample - 1 - j] =
+          s32 >> (8 * (outputBytesPerSample - 1 - j)) & 0xFF;
+    }
+
+    engine->Next();
+  }
+
+  std::ofstream output("output.raw", std::ofstream::binary);
+  output.write(pData, outputSamples * outputBytesPerSample);
+  output.close();
+
+  delete engine;
+  engine = nullptr;
+
+  return 0;
+}
